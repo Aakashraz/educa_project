c7666502e495106e1dfef370c25d3aebd4b3579f
Aakashraz <aakashraz@outlook.com>
2025-11-29 18:11:53 +0545

    models registered in the admin page

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Subject, Course, Module
 
-# Register your models here.
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}  # Automatically generates the slug field based on the title.
+    inlines = [ModuleInline]        # Embeds a related ModuleInline form directly within the course admin page.
+    # This means, when editing a course, admins can also add, edit, or delete associated modules without
+    # navigating away.
+
+
