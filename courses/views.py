dd9813c12bd601cee3d2037d9d179ca7714306f9
Aakashraz <aakashraz@outlook.com>
2025-12-22 18:52:34 +0545

    url for course_module_update added

--- courses/views.py
+++ courses/views.py
@@ -50,23 +50,39 @@ class CourseDeleteView(OwnerCourseMixin, DeleteView):
 
 
 class CourseModuleUpdateView(TemplateResponseMixin, View):
+    # TemplateResponseMixin: This mixin takes charge of rendering templates and returning an HTTP response.
+    # It requires a template_name attribute that indicates the template to be rendered and provides the
+    # render_to_response() method to pass it a context and render the template.
+    # View: The basic class-based view provided by Django.
+
     template_name = 'courses/manage/module/formset.html'
     course = None
 
     def get_formset(self, data=None):
         return ModuleFormSet(instance=self.course, data=data)
+    # get_formset(): You define this method to avoid repeating the code to build the formset. You
+    # create a ModuleFormSet object for the given Course object with optional data.
 
     def dispatch(self, request, pk):
         self.course = get_object_or_404(
             Course, id=pk, owner=self.request.user
         )
         return super().dispatch(request, pk)
+    # dispatch(): This method is provided by the View class. It takes an HTTP request and its parameters
+    # and attempts to delegate to a lowercase method that matches the HTTP method used. A GET request
+    # is delegated to the get() method and a POST request to post(), respectively. In this method, you
+    # use the get_object_or_404() shortcut function to get the Course object for the given id parameter
+    # that belongs to the current user. You include this code in the dispatch() method because you need to
+    # retrieve the course for both GET and POST requests. You save it into the course attribute of the view
+    # to make it accessible to other methods.
 
     def get(self, request, *args, **kwargs):
         formset = self.get_formset()
         return self.render_to_response(
             {'course': self.course, 'formset':formset}
         )
+    # get(): Executed for GET request. You build an empty ModuleFormSet formset and render it to the template
+    # together with the current Course object, using the render_to_response() method provided by TemplateResponseMixin.
 
     def post(self, request, *args, **kwargs):
         formset = self.get_formset(data=request.POST)
@@ -76,3 +92,10 @@ class CourseModuleUpdateView(TemplateResponseMixin, View):
         return self.render_to_response(
             {'course': self.course, 'formset':formset}
         )
+    # post(): Executed for POST requests. In this method, you perform the following actions:
+    # - You build a ModuleFormSet instance using the submitted data.
+    # - You execute the is_valid() method of the formset to validate all of its forms.
+    # - If the formset is valid, you save it by calling the save() method. At this point, any
+    #   changes made, such as adding, updating, or making modules for deletion, are applied to the
+    #   database. Then, you redirect users to the manage_course_list URL. If the formset is
+    #   not valid, you render the template to display any errors instead.
