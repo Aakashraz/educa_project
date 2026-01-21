38204540613c19f33ed09a8a42b1c6138f114443
Aakashraz <aakashraz@outlook.com>
2026-01-21 17:45:58 +0545

    student enrolled course list and detail views .html file added

--- students/views.py
+++ students/views.py
@@ -6,7 +6,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import CourseEnrollForm
-from course.models import Course
+from courses.models import Course
 
 
 
