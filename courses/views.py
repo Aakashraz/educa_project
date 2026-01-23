c3e9bb23d0413960d3b2dc68e6f033e03da88d66
Aakashraz <aakashraz@outlook.com>
2026-01-23 18:46:35 +0545

    django video embed installed

--- courses/views.py
+++ courses/views.py
@@ -137,7 +137,7 @@ class ContentCreateUpdateView(TemplateResponseMixin, View):
         self.model= self.get_model(model_name)
         if id:
             self.obj = get_object_or_404(
-                slef.model, id=id, owner=request.user
+                self.model, id=id, owner=request.user
             )
         return super().dispatch(request, module_id, model_name, id)
 
