68794c6d98787264f57d6e6793f9dafe549f4260
Aakashraz <aakashraz@outlook.com>
2026-02-08 23:27:03 +0545

    new pagination.py file added for using pagination class in SubjectListAPIView

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,5 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics
+from courses.api.pagination import StandardPagination
 from .serializers import SubjectSerializer
 from courses.models import Subject
 
@@ -8,6 +9,7 @@ from courses.models import Subject
 class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))    # The base QuerySet to use to retrieve objects
     serializer_class = SubjectSerializer    # The class to serializer objects
+    pagination_class = StandardPagination
 
 
 class SubjectDetailView(generics.RetrieveAPIView):
