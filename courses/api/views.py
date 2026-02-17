447fd5e3565b74facdad988d9b0dcf6a31ea0fee
Aakashraz <aakashraz@outlook.com>
2026-02-18 00:09:32 +0545

    pagination implemented and also checked, it is working

--- courses/api/views.py
+++ courses/api/views.py
@@ -10,6 +10,25 @@ class SubjectListView(generics.ListAPIView):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))    # The base QuerySet to use to retrieve objects
     serializer_class = SubjectSerializer    # The class to serializer objects
     pagination_class = StandardPagination
+#   This pagination line will result in different JSON structure returned by the view, as the following structure:
+# {
+#     "count": 4,
+#     "next": null,
+#     "previous": null,
+#     "results": [
+#         {
+#             "id": 1,
+#             "title": "Mathematics",
+#             ...
+#         },
+#         ...
+#     ]
+# }
+# The following items are now part of the JSON returned:
+# - count: The total number of results.
+# - next: The URL to retrieve the next page. The value is 'null' when there are no following pages.
+# - previous: The URL to retrieve the previous page. The value is 'null' when there are no previous pages.
+# - results: A list with the serialized 'objects' returned on this page.
 
 
 class SubjectDetailView(generics.RetrieveAPIView):
