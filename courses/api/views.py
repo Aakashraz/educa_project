693d7d9d1b3f18a4fb2d6e333b4433ff690c9216
Aakashraz <aakashraz@outlook.com>
2026-08-09 23:26:04 +0545

    minor change to debug--@action decrotor was not imported before

--- courses/api/views.py
+++ courses/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 
 from courses.api.pagination import StandardPagination
 from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
