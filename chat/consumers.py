60543cd692f7ba545072f39a1fb62119f1b0d8c9
Aakashraz <aakashraz@outlook.com>
2026-09-11 23:44:28 +0545

    added current user and datetime in both consumer.py and room.html

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,11 +1,13 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']    # retrieving the course_id from the scope
         self.room_group_name = f'chat_{self.id}'
 
@@ -22,10 +24,11 @@ class ChatConsumer(WebsocketConsumer):
             self.room_group_name, self.channel_name
         )
 
-    # receive message from WebSocket -- browser (client)
+    # receive message from WebSocket -- i.e., from the browser (client)
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
         # send message to WebSocket
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
@@ -33,9 +36,11 @@ class ChatConsumer(WebsocketConsumer):
                 'type': 'chat_message', # This is a special key that corresponds to the name of the method that
                 # should be invoked on consumers that receive the event.
                 'message': message,     # The actual message you are sending.
+                'user': self.user.username,
+                'datetime': now.isoformat(),
             }
         )
 
     def chat_message(self, event):
         # send message to WebSocket
-        self.send(text_data=json.dumps(event))
+        self.send(text_data=json.dumps(event))  # sends the WHOLE dict from self.room_group_name
