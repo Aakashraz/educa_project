d77d9e0801274ea532daae7a483b28f9063091b7
Aakashraz <aakashraz@outlook.com>
2026-09-14 00:42:34 +0545

    10 last messages shown in chat history, updated from last 5

--- chat/views.py
+++ chat/views.py
@@ -17,7 +17,7 @@ def course_chat_room(request, course_id):
     # retrieve chat history
     latest_messages = course.chat_messages.select_related(
         'user'
-    ).order_by('-id')[:5]
+    ).order_by('-id')[:10]
     latest_messages = reversed(latest_messages)
 
     return render(
