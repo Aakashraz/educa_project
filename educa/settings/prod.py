3ea26a05e177594e97e66bb4f160c834b448693d
Aakashraz <aakashraz@outlook.com>
2026-09-20 18:07:47 +0545

    nginx server setup in production environment

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -8,7 +8,7 @@ ADMINS = [
     ('jonadmin don', 'prodenv001@gmail.com')
 ]
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['educaproject.com', 'www.educaproject.com']
 
 DATABASES = {
     'default': {
@@ -20,3 +20,8 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+REDIS_URL = 'redis://redis:6379/1'
+CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
+# CACHES['default']['LOCATION'] = REDIS_URL
+# already set up in settings/base.py
